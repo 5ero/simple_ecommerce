4868116a54b0e979ad4f9e03611c29cc7303a8e4
Gareth Nicholas <gnicholas@imac.local>
2021-03-02 15:58:18 +0000

    added can create an order test

--- tests/Feature/OrdersTest.php
+++ tests/Feature/OrdersTest.php
@@ -4,22 +4,60 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\Customer;
 use Tests\TestCase;
 
 class OrdersTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
     
-    /** @Tests  */
+    /** @test  */
     public function can_create_an_order()
     {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $customer = Customer::factory()->create();
+
+        $product = [
+            'product_name' => $this->faker->sentence,
+            'product_description' => $this->faker->paragraph,
+            'product_price' => $this->faker->numberBetween($min = 1000, $max = 2000),
+            'product_qty' => $this->faker->numberBetween($min = 0, $max = 800),
+            'category_id' => 1
+        ];
+        $this->actingAs($user)->postJson(config('app.url').'/api/products', $product);
+
+        $p = Product::find(1);
+
         $order = Order::create([
-            'order_no' => 'BBL'.time(),
-            'products' => "['45','44','46']",
+            'order_no' => '100001',
+            'customer_id' => $customer->id,
+            'products' => [
+                [
+                    'product_id' => $p->id,
+                    'product' => $p->product_name,
+                    'product_price' => $p->product_price,
+                    'product_qty' => 5
+                ],
+                [
+                    'product_id' => $p->id,
+                    'product' => $p->product_name,
+                    'product_price' => $p->product_price,
+                    'product_qty' => 100
+                ],
+                
+            ],
+            'order_value' => $p->qty*$p->product_price,
             'status' => 'PAID',
-            'shipped' => '1',
-            'shipped_date' => '2021-01-29 14:30:22'
+            'shipped' => 0,
+            'shipped_date' => ''
         ]);
+
+        $this->actingAs($user)->get('/admin/orders')
+                ->assertSee('orders');
 
         
 
